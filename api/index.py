b4564e5ef679518e30f3fa17080b8287b67ad4e2
Ben Rhodes-Kropf <85074410+Benjamin-Rhodes-Kropf@users.noreply.github.com>
2024-03-01 15:05:36 -0500

    added pinecone

--- api/index.py
+++ api/index.py
@@ -1,6 +1,12 @@
 from flask import Flask, render_template
+from pinecone import Pinecone, ServerlessSpec, PodSpec
+import pinecone
+import torch
 
 app = Flask(__name__)
+
+pc = pinecone.Pinecone(api_key="b76cad94-53ae-4524-9156-b3450a92ce4a", environment="gcp-starter")
+index = pc.Index("dresses")
 
 @app.route('/')
 def home():
