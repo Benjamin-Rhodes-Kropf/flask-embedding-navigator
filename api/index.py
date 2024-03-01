4f2d95a27e312635371c432c3a6755f55ef9c73f
Ben Rhodes-Kropf <85074410+Benjamin-Rhodes-Kropf@users.noreply.github.com>
2024-03-01 12:26:00 -0500

    added grid and html template

--- api/index.py
+++ api/index.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -9,3 +9,11 @@ def home():
 @app.route('/about')
 def about():
     return 'About'
+
+# New route for the grid
+@app.route('/grid')
+def grid():
+    return render_template('grid.html')  # This will render the grid.html template
+
+if __name__ == '__main__':
+    app.run(debug=True)
