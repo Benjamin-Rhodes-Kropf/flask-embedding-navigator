fedf53e627b7f536920190a7e589b569464ee364
Ben Rhodes-Kropf <85074410+Benjamin-Rhodes-Kropf@users.noreply.github.com>
2024-03-01 23:09:32 -0500

    removed torch to reduce size

--- api/index.py
+++ api/index.py
@@ -1,17 +1,12 @@
 from flask import Flask, render_template, request, jsonify
 from pinecone import Pinecone
 import pinecone
-import torch
+import random
 
 app = Flask(__name__)
 
 pc = pinecone.Pinecone(api_key="b76cad94-53ae-4524-9156-b3450a92ce4a", environment="gcp-starter")
 index = pc.Index("dresses")
-
-# old home route
-# @app.route('/')
-# def home():
-#     return 'Hello, World!'
 
 @app.route('/', methods=['GET', 'POST'])
 def grid():
@@ -25,13 +20,12 @@ def grid():
                 top_k=9,
                 include_values=True
             )
-            # Prepare the data to send back to the client
             items = [{'link': match["id"], 'vector': match["values"]} for match in response["matches"]]
             return jsonify(items)
         else:
             return jsonify({'error': 'No vector provided'}), 400
     else:
-        vector = torch.rand(768).tolist()
+        vector = [random.random() for _ in range(768)]  # Modified line here
         response = index.query(
             vector=vector,
             top_k=9,
@@ -41,7 +35,6 @@ def grid():
         vectors = [match["values"] for match in response["matches"]]
         items = list(zip(links, vectors))
         return render_template('grid.html', items=items)
-
 
 if __name__ == '__main__':
     app.run(debug=True)
