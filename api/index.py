6e2143ed18c7a8c74b4cbf8f07de67d4f6dfbdbe
Ben Rhodes-Kropf <85074410+Benjamin-Rhodes-Kropf@users.noreply.github.com>
2024-03-01 21:40:30 -0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -8,55 +8,12 @@ app = Flask(__name__)
 pc = pinecone.Pinecone(api_key="b76cad94-53ae-4524-9156-b3450a92ce4a", environment="gcp-starter")
 index = pc.Index("dresses")
 
+# old home route
+# @app.route('/')
+# def home():
+#     return 'Hello, World!'
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
-
-@app.route('/about')
-def about():
-    return 'About'
-
-# # New route for the grid
-# @app.route('/grid', methods=['GET','POST'])
-# def grid(): 
-#     print("request received")
-#     print(f"request method: {request.method}")
-
-#     # Example of retrieving a vector from form data; adjust based on your actual data source
-#     if request.method == 'POST':
-#         data = request.get_json()  # Get the JSON data sent with the POST request
-#         vector = data.get('vector')  # Retrieve the vector from the JSON data
-#         print(f"vector: {vector}")
-#         if vector:
-#             vector = list(map(float, vector))
-#     else:
-#      vector = torch.rand(768).tolist()
-
-#     # Query the index
-#     print("Querying the index...")
-#     response = index.query(
-#         vector=vector,
-#         top_k=9,
-#         include_values=True
-#     )
-
-#     for match in response["matches"]:
-#         print(match["id"])
-
-#     # get all links in response
-#     links = []
-#     vectors = []
-#     for match in response["matches"]:
-#         links.append(match["id"])
-#         vectors.append(match["values"])
-    
-#     # Create a list of tuples where each tuple is (link, vector)
-#     items = list(zip(links, vectors))
-
-#     return render_template('grid.html', items=items)  # Pass the links to the template
-
-@app.route('/grid', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def grid():
     if request.method == 'POST':
         data = request.get_json()
