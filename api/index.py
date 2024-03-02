60c04f6416c168240bf8b9d9111992619f7c9ca0
Ben Rhodes-Kropf <85074410+Benjamin-Rhodes-Kropf@users.noreply.github.com>
2024-03-01 21:25:08 -0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -22,28 +22,27 @@ def about():
 def grid(): 
     print("request received")
     print(f"request method: {request.method}")
-    print(f"request form: {request.form}")
-    print(f"request vector: {request.form.get('vector')}")
-    print(f"request args: {request.args}")
 
     # Example of retrieving a vector from form data; adjust based on your actual data source
     if request.method == 'POST':
-        # Example of retrieving a vector from form data; adjust based on your actual data source
-        input_vector = request.form.get('vector')
-        if input_vector:
-            # Assuming input_vector is a string of comma-separated numbers
-            vector = list(map(float, input_vector.split(',')))
-        else:
-            vector = torch.rand(768).tolist()
+        data = request.get_json()  # Get the JSON data sent with the POST request
+        vector = data.get('vector')  # Retrieve the vector from the JSON data
+        print(f"vector: {vector}")
+        if vector:
+            vector = list(map(float, vector))
     else:
-        # For GET requests or if no vector is provided
-        vector = torch.rand(768).tolist()
+     vector = torch.rand(768).tolist()
 
+    # Query the index
+    print("Querying the index...")
     response = index.query(
         vector=vector,
         top_k=9,
         include_values=True
     )
+
+    for match in response["matches"]:
+        print(match["id"])
 
     # get all links in response
     links = []
